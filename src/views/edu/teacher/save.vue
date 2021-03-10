40f6ae8c16ebfca6c04f2a13d5f694475612d0e5
lyuly <liumike567@gmail.com>
2021-03-10 20:59:42 +0800

    update

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -47,25 +47,25 @@ export default {
       saveBtnDisabled: false // 保存按钮是否禁用,
     }
   },
-  created() { // 页面渲染之前执行
-    this.init()    
-  },
-  watch: {  // 监听
-    $route(to, from) {  // 路由变化方式，路由发生变化，方法就会执行
+  watch: { // 监听
+    $route(to, from) { // 路由变化方式，路由发生变化，方法就会执行
       this.init()
     }
+  },
+  created() { // 页面渲染之前执行
+    this.init()
   },
   methods: {
     init() {
       // 判断路径是否有id值,做修改
-    if (this.$route.params && this.$route.params.id) {
-      // 从路径获取id值
-      const id = this.$route.params.id
-      // 调用根据id查询的方法
-      this.getInfo(id)
-    } else {  // 路径没有id值,做添加
-      // 清空表单
-      this.teacher = {}
+      if (this.$route.params && this.$route.params.id) {
+        // 从路径获取id值
+        const id = this.$route.params.id
+        // 调用根据id查询的方法
+        this.getInfo(id)
+      } else { // 路径没有id值,做添加
+        // 清空表单
+        this.teacher = {}
       }
     },
     // 根据讲师id查询的方法
