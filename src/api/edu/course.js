c2bce076df96abace56eb14f880ea4b2afd8875e
lyuly <liumike567@gmail.com>
2021-03-23 15:37:13 +0800

    update

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+
 export default {
   // 1 添加课程信息
   addCourseInfo(courseInfo) {
@@ -28,6 +29,29 @@ export default {
       url: '/eduservice/course/updateCourseInfo',
       method: 'post',
       data: courseInfo
+    })
+  },
+  // 课程确认信息显示
+  getPublishCourseInfo(id) {
+    return request({
+      url: '/eduservice/course/getPublishCourseInfo/' + id,
+      method: 'get'
+    })
+  },
+  // 课程最终发布
+  publishCourse(id) {
+    return request({
+      url: '/eduservice/course/publishCourse/' + id,
+      method: 'post'
+    })
+  },
+  // TODO 课程列表
+
+  // 课程最终发布
+  getListCourse() {
+    return request({
+      url: '/eduservice/course',
+      method: 'get'
     })
   }
 }
